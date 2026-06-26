9a33a5b840e40406a0d45e67eeb7e1983729dbc3
acregan <hello@anthonycregan.co.uk>
2026-06-26 14:50:26 +0800

    feat(core): add ArticleContributor type and contributors/tags fields to Article
    Add ArticleContributor interface with did, role, and displayName fields.
    Add contributors and tags to the Article interface and RawDocument fetch shape.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
-export type { ArticleRef, SiteGroup, Site, Article, SiteRecord, ArticleResult } from "./types.js";
+export type { ArticleRef, SiteGroup, Site, Article, ArticleContributor, SiteRecord, ArticleResult } from "./types.js";
 export { fetchSite, fetchArticle, fetchArticleBySlug, resolvePublicationUri } from "./fetch.js";
 export { listSites, listArticles } from "./list.js";
 export { toSlug, slugFromUri, flattenArticles } from "./utils.js";
